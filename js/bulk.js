ccf393dd40a2936809c49b4745941eb5b31e85bf
Brandon Kalinowski <brandon@brandonkalinowski.com>
2022-09-03 18:08:30 -0700

    formatting, web text and remove analytics

--- js/bulk.js
+++ js/bulk.js
@@ -1,56 +1,66 @@
 // Plausible Analytics
-window.plausible = window.plausible || function () { (window.plausible.q = window.plausible.q || []).push(arguments) }
+window.plausible = window.plausible || function () {} // Analytics disabled
 
 // Button links
 document.querySelectorAll('.link-btn').forEach(function (el) {
-    el.addEventListener('click', function () {
-        window.open(el.getAttribute('data-url'), '_blank')
-    })
+  el.addEventListener('click', function () {
+    window.open(el.getAttribute('data-url'), '_blank')
+  })
 })
 
 // Process URL after clicking 'Clean Links' button
-document.getElementById('link-clean-btn').addEventListener('click', function () {
+document
+  .getElementById('link-clean-btn')
+  .addEventListener('click', function () {
     plausible('Bulk Clean Link')
     // Read settings
-    var youtubeShortenEnabled = document.getElementById('youtube-shorten-check').checked
+    var youtubeShortenEnabled = document.getElementById(
+      'youtube-shorten-check'
+    ).checked
     var vxTwitterEnabled = document.getElementById('vxTwitter-check').checked
     // Split comma-separated or newline-seperated input into array and trim whitespace
-    var oldLinks = document.getElementById('link-bulk-input').value.split(/\n|\,/)
+    var oldLinks = document
+      .getElementById('link-bulk-input')
+      .value.split(/\n|\,/)
     // Filter out blank lines
-    oldLinks = oldLinks.filter(link => link.length > 0)
+    oldLinks = oldLinks.filter((link) => link.length > 0)
     // Clean links
     var newLinks = []
     oldLinks.forEach((link) => {
-        var processedLink = cleanLink(link, youtubeShortenEnabled, vxTwitterEnabled)
-        newLinks.push(processedLink)
+      var processedLink = cleanLink(
+        link,
+        youtubeShortenEnabled,
+        vxTwitterEnabled
+      )
+      newLinks.push(processedLink)
     })
     // Output result
-    var result = newLinks.toString().replaceAll(',','\n')
+    var result = newLinks.toString().replaceAll(',', '\n')
     document.getElementById('link-copy-btn').removeAttribute('disabled')
     document.getElementById('link-bulk-output').value = result
-})
+  })
 
 // Copy link button
 document.getElementById('link-copy-btn').addEventListener('click', function () {
-    var btn = document.getElementById('link-copy-btn')
-    if (navigator.clipboard) {
-        // Use Clipboard API if available
-        var copyText = document.getElementById('link-bulk-output').value
-        navigator.clipboard.writeText(copyText)
-    } else {
-        // Fallback to older API
-        var copyText = document.getElementById('link-bulk-output')
-        copyText.select()
-        document.execCommand('copy')
-    }
-    // Change button design
-    btn.classList.remove('btn-outline-primary')
-    btn.classList.add('btn-outline-success')
-    btn.innerHTML = '<i class="bi bi-check"></i> Copied'
-    // Revert after three seconds
-    setTimeout(function () {
-        btn.classList.remove('btn-outline-success')
-        btn.classList.add('btn-outline-primary')
-        btn.innerHTML = '<i class="bi bi-clipboard"></i> Copy to Clipboard'
-    }, 2000)
+  var btn = document.getElementById('link-copy-btn')
+  if (navigator.clipboard) {
+    // Use Clipboard API if available
+    var copyText = document.getElementById('link-bulk-output').value
+    navigator.clipboard.writeText(copyText)
+  } else {
+    // Fallback to older API
+    var copyText = document.getElementById('link-bulk-output')
+    copyText.select()
+    document.execCommand('copy')
+  }
+  // Change button design
+  btn.classList.remove('btn-outline-primary')
+  btn.classList.add('btn-outline-success')
+  btn.innerHTML = '<i class="bi bi-check"></i> Copied'
+  // Revert after three seconds
+  setTimeout(function () {
+    btn.classList.remove('btn-outline-success')
+    btn.classList.add('btn-outline-primary')
+    btn.innerHTML = '<i class="bi bi-clipboard"></i> Copy to Clipboard'
+  }, 2000)
 })
--- js/main.js
+++ js/main.js
@@ -1,149 +1,172 @@
-
 // Plausible Analytics
-window.plausible = window.plausible || function () { (window.plausible.q = window.plausible.q || []).push(arguments) }
+window.plausible = window.plausible || function () {}
 
 // Detect iOS
 // Credit: https://stackoverflow.com/a/9039885
 function ifiOS() {
-    if (['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.userAgent)) {
-        return true
-    } else if (navigator.userAgent.includes('Mac') && 'ontouchend' in document) {
-        // iPadOS detection
-        return true
-    } else {
-        return false
-    }
+  if (
+    [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod',
+    ].includes(navigator.userAgent)
+  ) {
+    return true
+  } else if (navigator.userAgent.includes('Mac') && 'ontouchend' in document) {
+    // iPadOS detection
+    return true
+  } else {
+    return false
+  }
 }
 
 // Function for cleaning link
 function processLink(link) {
-    plausible('Clean Link')
-    var youtubeShortenEnabled = document.getElementById('youtube-shorten-check').checked
-    var vxTwitterEnabled = document.getElementById('vxTwitter-check').checked
-    var newLink = cleanLink(link, youtubeShortenEnabled, vxTwitterEnabled)
-    // Switch to output
-    document.getElementById('link-output').value = newLink
-    document.getElementById('initial').style.display = 'none'
-    document.getElementById('completed').style.display = 'block'
-    // Highlight the output for easy copy
-    document.getElementById('link-output').select()
+  plausible('Clean Link')
+  var youtubeShortenEnabled = document.getElementById(
+    'youtube-shorten-check'
+  ).checked
+  var vxTwitterEnabled = document.getElementById('vxTwitter-check').checked
+  var newLink = cleanLink(link, youtubeShortenEnabled, vxTwitterEnabled)
+  // Switch to output
+  document.getElementById('link-output').value = newLink
+  document.getElementById('initial').style.display = 'none'
+  document.getElementById('completed').style.display = 'block'
+  // Highlight the output for easy copy
+  document.getElementById('link-output').select()
 }
 
 // Process URL after a paste action is detected
 document.getElementById('link-input').addEventListener('paste', function () {
-    // This is wrapped in a timeout or it executes before the value has changed
-    setTimeout(function () {
-        processLink(document.getElementById('link-input').value)
-    }, 50)
+  // This is wrapped in a timeout or it executes before the value has changed
+  setTimeout(function () {
+    processLink(document.getElementById('link-input').value)
+  }, 50)
 })
 
 // Paste button
-if (typeof navigator.clipboard.readText !== "undefined") {
-    document.getElementById('link-paste-btn').addEventListener('click', function () {
-        navigator.clipboard.readText().then(function (data) {
-            processLink(data)
-        })
+if (typeof navigator.clipboard.readText !== 'undefined') {
+  document
+    .getElementById('link-paste-btn')
+    .addEventListener('click', function () {
+      navigator.clipboard.readText().then(function (data) {
+        processLink(data)
+      })
     })
 } else {
-    document.getElementById('link-paste-btn').disabled = true
+  document.getElementById('link-paste-btn').disabled = true
 }
 
 // Process URL after clicking arrow button
 document.getElementById('link-submit').addEventListener('click', function () {
-    processLink(document.getElementById('link-input').value)
+  processLink(document.getElementById('link-input').value)
 })
 
 // Copy link button
 document.getElementById('link-copy-btn').addEventListener('click', function () {
-    var btn = document.getElementById('link-copy-btn')
-    if (navigator.clipboard) {
-        // Use Clipboard API if available
-        var copyText = document.getElementById('link-output').value
-        navigator.clipboard.writeText(copyText)
-    } else {
-        // Fallback to older API
-        var copyText = document.getElementById('link-output')
-        copyText.select()
-        document.execCommand('copy')
-    }
-    // Change button design
-    btn.classList.remove('btn-primary')
-    btn.classList.add('btn-success')
-    btn.innerHTML = '<i class="bi bi-check"></i> Copied'
-    // Revert after three seconds
-    setTimeout(function () {
-        btn.classList.remove('btn-success')
-        btn.classList.add('btn-primary')
-        btn.innerHTML = '<i class="bi bi-clipboard"></i> Copy to Clipboard'
-    }, 2000)
+  var btn = document.getElementById('link-copy-btn')
+  if (navigator.clipboard) {
+    // Use Clipboard API if available
+    var copyText = document.getElementById('link-output').value
+    navigator.clipboard.writeText(copyText)
+  } else {
+    // Fallback to older API
+    var copyText = document.getElementById('link-output')
+    copyText.select()
+    document.execCommand('copy')
+  }
+  // Change button design
+  btn.classList.remove('btn-primary')
+  btn.classList.add('btn-success')
+  btn.innerHTML = '<i class="bi bi-check"></i> Copied'
+  // Revert after three seconds
+  setTimeout(function () {
+    btn.classList.remove('btn-success')
+    btn.classList.add('btn-primary')
+    btn.innerHTML = '<i class="bi bi-clipboard"></i> Copy to Clipboard'
+  }, 2000)
 })
 
 // Start over button
-document.getElementById('link-startover').addEventListener('click', function () {
+document
+  .getElementById('link-startover')
+  .addEventListener('click', function () {
     document.getElementById('completed').style.display = 'none'
     document.getElementById('initial').style.display = 'block'
     document.getElementById('link-input').value = ''
     document.getElementById('link-input').select()
-})
+  })
 
 // Share button
 if (navigator.canShare) {
-    document.getElementById('link-share-btn').addEventListener('click', function () {
-        try {
-            navigator.share({
-                url: document.getElementById('link-output').value
-            })
-        }
-        catch (e) {
-            alert('There was an error:\n\n' + e.message)
-        }
+  document
+    .getElementById('link-share-btn')
+    .addEventListener('click', function () {
+      try {
+        navigator.share({
+          url: document.getElementById('link-output').value,
+        })
+      } catch (e) {
+        alert('There was an error:\n\n' + e.message)
+      }
     })
 } else {
-    document.getElementById('link-share-btn').disabled = true
+  document.getElementById('link-share-btn').disabled = true
 }
 
 // QR Code button
 // This loads the QR code image only when the button is pressed
 var qrModal = document.getElementById('qr-modal')
 qrModal.addEventListener('show.bs.modal', function (event) {
-    var currentLink = document.getElementById('link-output').value
-    var qrElement =  document.getElementById('qr-img')
-    var qrSrc = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=' + encodeURIComponent(currentLink)
-    if (qrElement.src != qrSrc) {
-        qrElement.setAttribute('src', qrSrc)
-        qrElement.setAttribute('title', currentLink)
-    }
+  var currentLink = document.getElementById('link-output').value
+  var qrElement = document.getElementById('qr-img')
+  var qrSrc =
+    'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=' +
+    encodeURIComponent(currentLink)
+  if (qrElement.src != qrSrc) {
+    qrElement.setAttribute('src', qrSrc)
+    qrElement.setAttribute('title', currentLink)
+  }
 })
 
 // Email button
 document.getElementById('email-btn').addEventListener('click', function () {
-    var currentLink = document.getElementById('link-output').value
-    var emailSubject = 'Link for you'
-    var emailBody = currentLink + '\n\nSent with Link Cleaner: linkcleaner.app'
-    window.open('mailto:?subject=' + encodeURIComponent(emailSubject) + '&body=' + encodeURIComponent(emailBody), '_blank')
+  var currentLink = document.getElementById('link-output').value
+  var emailSubject = 'Link for you'
+  var emailBody = currentLink + '\n\nSent with Link Cleaner: linkcleaner.app'
+  window.open(
+    'mailto:?subject=' +
+      encodeURIComponent(emailSubject) +
+      '&body=' +
+      encodeURIComponent(emailBody),
+    '_blank'
+  )
 })
 
 // Button links
 document.querySelectorAll('.link-btn').forEach(function (el) {
-    el.addEventListener('click', function () {
-        window.open(el.getAttribute('data-url'), '_blank')
-    })
+  el.addEventListener('click', function () {
+    window.open(el.getAttribute('data-url'), '_blank')
+  })
 })
 
 // Show Shortcut prompt on iOS
 if (ifiOS()) {
-    document.getElementById('apple-shortcut-btn').style.display = 'block'
+  document.getElementById('apple-shortcut-btn').style.display = 'block'
 }
 
 // Support for Web Share Target API, Siri Shortcut, and OpenSearch
 const parsedUrl = new URL(window.location)
 if (parsedUrl.searchParams.get('url')) {
-    // This is where the URL SHOULD BE
-    processLink(parsedUrl.searchParams.get('url'))
+  // This is where the URL SHOULD BE
+  processLink(parsedUrl.searchParams.get('url'))
 } else if (parsedUrl.searchParams.get('text')) {
-    // Android usually puts URLs here
-    processLink(parsedUrl.searchParams.get('text'))
+  // Android usually puts URLs here
+  processLink(parsedUrl.searchParams.get('text'))
 } else if (parsedUrl.searchParams.get('title')) {
-    // Android sometimes puts URLs here
-    processLink(parsedUrl.searchParams.get('title'))
+  // Android sometimes puts URLs here
+  processLink(parsedUrl.searchParams.get('title'))
 }
